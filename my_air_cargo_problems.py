3b2dfc2ca017bfb24d83947e19312169dbf677e6
chuongdao <cd.kernel.dev.105@gmail.com>
2017-06-08 00:30:31 -0700

    finish air_cargo_2

--- my_air_cargo_problems.py
+++ my_air_cargo_problems.py
@@ -282,6 +282,55 @@ def air_cargo_p2() -> AirCargoProblem:
     # TODO implement Problem 2 definition
     cargos = ['C1', 'C2', 'C3']
     planes = ['P1', 'P2', 'P3']
+    airports = ['SFO','JFK','ATL']
+    pos = [
+            expr('At(C1,SFO)'),
+            expr('At(C2,JFK)'),
+            expr('At(C3,ATL)'),
+            expr('At(P1,SFO)'),
+            expr('At(P2,JFK)'),
+            expr('At(P3,ATL)')
+          ]
+    neg = [
+            expr('At(C1,JFK)'),
+            expr('At(C1,SFO)'),
+            
+            expr('At(C2,SFO)'),
+            expr('At(C2,ATL)'),
+            
+            expr('At(C3,SFO)'),
+            expr('At(C3,JFK)'),
+            
+            expr('In(C1,P1)'),
+            expr('In(C1,P2)'),
+            expr('In(C1,P3)'),
+            
+            expr('In(C2,P1)'),
+            expr('In(C2,P2)'),
+            expr('In(C2,P3)'),
+            
+            expr('In(C3,P1)'),
+            expr('In(C3,P2)'),
+            expr('In(C3,P3)'),
+            
+            expr('At(P1,JFK)'),
+            expr('At(P1,ATL)'),
+            
+            expr('At(P2,SFO)'),
+            expr('At(P2,ATL)'),
+            
+            expr('At(P3,SFO)'),
+            expr('At(P3,JFK)')
+          ]
+
+    goal = [
+
+            expr('At(C1,JFK)'),
+            expr('At(C2,SFO)'),
+            expr('At(C3,SFO)')
+        ]
+    initial_state = FluentState(pos, neg)
+    return AirCargoProblem(cargos, planes, airports, initial_state, goal)
 
 
 def air_cargo_p3() -> AirCargoProblem:
